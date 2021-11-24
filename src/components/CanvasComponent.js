8c8b4f3a268831039aa2872fee77d95507b4a07b
Keidusii <mhallii@umich.edu>
2021-11-24 16:28:57 -0500

    removed props from canvas component

--- src/components/CanvasComponent.js
+++ src/components/CanvasComponent.js
@@ -5,7 +5,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEdit } from '@fortawesome/free-solid-svg-icons';
 import { Input } from 'reactstrap';
 
-function Canvas(props) {
+function Canvas() {
     const canvasRef = useRef();
     const [color, setColor] = useState("black");
     const [lineWidth, setLineWidth] = useState(1);
@@ -53,8 +53,6 @@ function Canvas(props) {
             </div>
             <CanvasDraw
                 ref={canvasRef}
-                canvasHeight={window.innerWidth > 430 ? 400 : 250}
-                canvasWidth={window.innerWidth > 430 ? 400 : 250}
                 brushColor={color}
                 brushRadius={lineWidth}
                 hideGrid={true}
@@ -74,93 +72,6 @@ function Canvas(props) {
             />
         </>
     );
-    /*const canvasRef = useRef(null);
-    const contextRef = useRef(null);
-
-    const [drawing, setDrawing] = useState(false);
-    const [position, setPosition] = useState({
-        x: 0,
-        y: 0
-    })
-
-    useEffect(() => {
-        const canvas = canvasRef.current;
-        const context = canvas.getContext('2d');
-        canvas.style.height = "480px";
-        canvas.style.width = "100%";
-        resizeCanvasToDisplaySize(canvas);
-        resizeCanvas(canvas);
-        context.strokeStyle = props.color;
-        context.lineCap = "round";
-        context.lineJoin = "round";
-        context.lineWidth = props.lineWidth;
-        contextRef.current = context;
-    }, [props.color, props.lineWidth])
-
-    const resizeCanvasToDisplaySize = canvas => {
-    
-        const { width, height } = canvas.getBoundingClientRect()
-
-        if (canvas.width !== width || canvas.height !== height) {
-          canvas.width = width
-          canvas.height = height
-          return true;
-        }
-    
-        return false
-    }
-
-    function resizeCanvas(canvas) {
-        const { width, height } = canvas.getBoundingClientRect()
-        
-        if (canvas.width !== width || canvas.height !== height) {
-          const { devicePixelRatio:ratio=1 } = window
-          const context = canvas.getContext('2d')
-          canvas.width = width*ratio
-          canvas.height = height*ratio
-          context.scale(ratio, ratio)
-          return true
-        }
-    
-        return false
-      }
-
-    const startDrawing = ({nativeEvent}) => {
-        const {offsetX, offsetY} = nativeEvent;
-        setPosition({ x: offsetX, y: offsetY });
-        contextRef.current.beginPath();
-        contextRef.current.moveTo(position.x, position.y);
-        setDrawing(true);
-    }
-
-    const stopDrawing = () => {
-        contextRef.current.beginPath();
-        setDrawing(false);
-    }
-
-    const draw = ({nativeEvent}) => {
-        if (!drawing) {
-            return;
-        }
-        const {offsetX, offsetY} = nativeEvent;
-        setPosition({ x: offsetX, y: offsetY });
-        contextRef.current.lineTo(position.x, position.y);
-        contextRef.current.stroke();
-    }
-
-    return (
-        <canvas
-            id="canvas" 
-            className="mt-2"
-            onMouseDown={startDrawing}
-            onTouchStart={startDrawing}
-            onMouseUp={stopDrawing}
-            onTouchEnd={stopDrawing}
-            onMouseMove={draw}
-            onTouchMove={draw}
-            onMouseLeave={stopDrawing}
-            ref={canvasRef}
-        />
-    );*/
 }
+
 export default Canvas;
